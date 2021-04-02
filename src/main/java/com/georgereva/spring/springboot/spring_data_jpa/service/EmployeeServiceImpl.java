8526fa751afea528728af31cac101e79fb23101b
georgereva <joravartan@gmail.com>
2021-04-02 06:52:48 +0300

    Initial commit

---
+++ src/main/java/com/georgereva/spring/springboot/spring_data_jpa/service/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package com.georgereva.spring.springboot.spring_data_jpa.service;
+
+import com.georgereva.spring.springboot.spring_data_jpa.dao.EmployeeRepository;
+import com.georgereva.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Employee employee = null;
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+
+        if (optionalEmployee.isPresent()) {
+            employee = optionalEmployee.get();
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Employee> findAllByName(String string) {
+        List<Employee> employees = employeeRepository.findAllByName(string);
+        return employees;
+    }
+}
